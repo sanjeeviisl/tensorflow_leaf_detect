03214b45f8e04be377ac6930b297ae4be362b66f
Sanjeev <sanjeev@iisl.co.in>
2019-11-25 15:52:30 +0530

    updated

--- tensorflow_ssd/object_detection/export_tflite_ssd_graph.py
+++ tensorflow_ssd/object_detection/export_tflite_ssd_graph.py
@@ -9,7 +9,7 @@ flags.DEFINE_string(
     'pipeline_config_path',"model/ssd_mobilenet_v2/pipeline.config" ,
     'Path to a pipeline_pb2.TrainEvalPipelineConfig config '
     'file.')
-flags.DEFINE_string('trained_checkpoint_prefix', "data/model.ckpt-8752", 'Checkpoint prefix.')
+flags.DEFINE_string('trained_checkpoint_prefix', "data/model.ckpt-5197", 'Checkpoint prefix.')
 flags.DEFINE_integer('max_detections', 4,
                      'Maximum number of detections (boxes) to show.')
 
